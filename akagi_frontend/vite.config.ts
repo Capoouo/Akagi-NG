11d274b4ef25db4961f116898e25499d2aa5bb57
Xe-Persistent <474663082@qq.com>
2026-03-19 14:57:57 +0800

    :heavy_minus_sign: Remove vite-plugin-compression

--- akagi_frontend/vite.config.ts
+++ akagi_frontend/vite.config.ts
@@ -2,20 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteCompression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   base: './',
   build: {
     target: 'esnext',
